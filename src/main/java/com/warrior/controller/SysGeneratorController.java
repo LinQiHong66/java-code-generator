30e8c66d9e24eb966021168a2c2bda01572ad65d
linqh03 <245393886@qq.com>
2020-12-03 18:45:52 +0800

    init

---
+++ src/main/java/com/warrior/controller/SysGeneratorController.java
@@ -0,0 +1,58 @@
+package com.warrior.controller;
+
+import com.warrior.service.SysGeneratorService;
+import com.warrior.common.utils.Query;
+import com.warrior.common.utils.R;
+import com.warrior.common.utils.PageUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 代码生成器
+ */
+@Controller
+@RequestMapping("/sys/generator")
+public class SysGeneratorController {
+	@Autowired
+	private SysGeneratorService sysGeneratorService;
+	
+	/**
+	 * 列表
+	 */
+	@ResponseBody
+	@RequestMapping("/list")
+	public R list(@RequestParam Map<String, Object> params){
+		//查询列表数据
+		Query query = new Query(params);
+		List<Map<String, Object>> list = sysGeneratorService.queryList(query);
+		int total = sysGeneratorService.queryTotal(query);
+		
+		PageUtils pageUtil = new PageUtils(list, total, query.getLimit(), query.getPage());
+		
+		return R.ok().put("page", pageUtil);
+	}
+	
+	/**
+	 * 生成代码
+	 */
+	@RequestMapping("/code")
+	public void code(String tables, HttpServletResponse response) throws IOException{
+		byte[] data = sysGeneratorService.generatorCode(tables.split(","));
+		
+		response.reset();  
+        response.setHeader("Content-Disposition", "attachment; filename=\"generator.zip\"");
+        response.addHeader("Content-Length", "" + data.length);  
+        response.setContentType("application/octet-stream; charset=UTF-8");  
+  
+        IOUtils.write(data, response.getOutputStream());  
+	}
+}
